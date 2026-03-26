fecbe81f0e37e8be2236ff6970cea6c51e8ebbe6
mounika <ranjithreddy0352@gmail.com>
2026-03-26 11:07:53 +0530

    Updated Config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '127.0.0.1',
+    host: true,
     port: 5173,
     strictPort: true,
+    allowedHosts:true,
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8000',
